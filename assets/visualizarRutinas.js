653f109cc4bc42d1e25e686762620826409ee49c
yago <a22yagodj@iessanclemente.net>
2024-06-06 19:32:42 +0200

    estilo

--- assets/visualizarRutinas.js
+++ assets/visualizarRutinas.js
@@ -9,10 +9,10 @@ configuracion.addEventListener("click", (e) => {
     if (e.target.className == "config") {
         if (oculto == true) {
             oculto = false;
-            opciones.style.visibility="visible";
+            opciones.style.visibility = "visible";
         } else {
             oculto = true;
-            opciones.style.visibility="hidden";
+            opciones.style.visibility = "hidden";
         }
 
     }
@@ -45,44 +45,44 @@ btnFiltro.addEventListener("click", (e) => {
 
 //Animación botones
 
-document.body.addEventListener("mouseover", (e) => { 
-    if (e.target.tagName == "BUTTON" && e.target.className!="config" && e.target.classList.contains("navbar-toggler")!=true) {
+document.body.addEventListener("mouseover", (e) => {
+    if (e.target.tagName == "BUTTON" && e.target.className != "config" && e.target.classList.contains("navbar-toggler") != true) {
         e.target.style.backgroundColor = "orange";
-    } 
+    }
 })
 
-document.body.addEventListener("mouseout", (e) => {  
-    if (e.target.tagName == "BUTTON" && e.target.className!="config" && e.target.classList.contains("navbar-toggler")!=true) {
+document.body.addEventListener("mouseout", (e) => {
+    if (e.target.tagName == "BUTTON" && e.target.className != "config" && e.target.classList.contains("navbar-toggler") != true) {
         e.target.style.backgroundColor = "white";
-    } 
+    }
 })
 
 
 //Ampliar cartas
 
 function getCardElement(target) {
-            if (target.classList.contains("card")) {
-                return target;
-            } else if (target.parentElement && target.parentElement.classList.contains("card")) {
-                return target.parentElement;
-            } else if (target.parentElement && target.parentElement.parentElement && target.parentElement.parentElement.classList.contains("card")) {
-                return target.parentElement.parentElement;
-            }
-            return null;
-        }
+    if (target.classList.contains("card")) {
+        return target;
+    } else if (target.parentElement && target.parentElement.classList.contains("card")) {
+        return target.parentElement;
+    } else if (target.parentElement && target.parentElement.parentElement && target.parentElement.parentElement.classList.contains("card")) {
+        return target.parentElement.parentElement;
+    }
+    return null;
+}
 
-        document.body.addEventListener("mouseover", (e) => {
-            const cardElement = getCardElement(e.target);
-            if (cardElement) {
-                cardElement.style.transform = "scale(1.15)";
-                cardElement.style.zIndex = "1";
-            }
-        });
+document.body.addEventListener("mouseover", (e) => {
+    const cardElement = getCardElement(e.target);
+    if (cardElement) {
+        cardElement.style.transform = "scale(1.15)";
+        cardElement.style.zIndex = "1";
+    }
+});
 
-        document.body.addEventListener("mouseout", (e) => {
-            const cardElement = getCardElement(e.target);
-            if (cardElement) {
-                cardElement.style.transform = "scale(1)";
-                cardElement.style.zIndex = "0";
-            }
-        });
+document.body.addEventListener("mouseout", (e) => {
+    const cardElement = getCardElement(e.target);
+    if (cardElement) {
+        cardElement.style.transform = "scale(1)";
+        cardElement.style.zIndex = "0";
+    }
+});
