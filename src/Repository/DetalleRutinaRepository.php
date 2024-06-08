76a6e3071404852b74d226a59b9e5deb8acec69e
yago <a22yagodj@iessanclemente.net>
2024-06-08 23:24:18 +0200

    correccion info exercise

--- src/Repository/DetalleRutinaRepository.php
+++ src/Repository/DetalleRutinaRepository.php
@@ -38,7 +38,7 @@ class DetalleRutinaRepository extends ServiceEntityRepository
             ->setParameter('ejercicio', $ejercicio)
             ->getQuery()
             ->getResult();
-        return array($exercises);
+        return array_reverse($exercises);
     }
 
     public function remove(DetalleRutina $detalle): void
